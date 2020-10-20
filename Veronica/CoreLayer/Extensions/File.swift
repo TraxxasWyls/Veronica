1c2f40562ec11054a977746dc7efe10feb1c3ac4
Dmitry <savinov.dsta@gmail.com>
2020-10-20 10:10:30 +0300

    синтаксис чуть исправлен

--- Veronica/CoreLayer/Extensions/File.swift
+++ Veronica/CoreLayer/Extensions/File.swift
@@ -13,8 +13,8 @@ extension File {
     /// Reafactoring content inside the file
     /// - Parameter replacement: dictionary of words that needs to be changed with another options in File
     func refactorContent(with replacement: [String: String]) {
-            let fileData = try? readAsString()
-            let refactoredfileData = fileData?.replacingOccurrences(with: replacement)
-            try? write(refactoredfileData.unwrap())
+        let fileData = try? readAsString()
+        let refactoredfileData = fileData?.replacingOccurrences(with: replacement)
+        try? write(refactoredfileData.unwrap())
     }
 }
