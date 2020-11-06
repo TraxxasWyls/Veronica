5c9de2c0837ab0e0ced8c0029322bdcf49e7b37d
Dmitry <savinov.dsta@gmail.com>
2020-11-06 19:37:39 +0300

    много всякого

--- Extensions/FolderRenamer.swift
+++ Extensions/FolderRenamer.swift
@@ -13,12 +13,20 @@ extension Folder {
     /// - Throws: `LocationError` if the item couldn't be renamed.
    public func renameAll(with replacement: [String: String]) throws {
         try rename(to: name.replacingOccurrences(with: replacement))
+        try renameFiles(with: replacement)
         try subfolders.recursive.forEach { folder in
             try folder.rename(to: folder.name.replacingOccurrences(with: replacement))
-            try folder.files.enumerated().forEach { (index, file) in
-                try file.rename(to: file.name.replacingOccurrences(with: replacement))
-                file.refactorContent(with: replacement)
-            }
+            try folder.renameFiles(with: replacement)
+        }
+    }
+
+    /// Renaming all flles names, also refactoring content inside each file (without subfolders and their files)
+    /// - Parameter replacement: dictionary of words that needs to be changed with another options
+    /// - Throws: `LocationError` if the item couldn't be renamed.
+    public func renameFiles(with replacement: [String: String]) throws {
+        try files.enumerated().forEach { (index, file) in
+            try file.rename(to: file.name.replacingOccurrences(with: replacement))
+            file.refactorContent(with: replacement)
         }
     }
 }
